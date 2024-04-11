cfd4c5c77d80a363ed12399463cabce586cc29f2
stevejuan <predastefanmarian@gmail.com>
2024-04-11 23:19:47 +0300

    Register Poco

---
+++ Application/Pocos/RegisterPoco.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Pocos;
+
+public class RegisterPoco
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string ConfirmPassword { get; set; }
+    public required string PostalCode { get; set; }
+    public required string Address_1 { get; set; }
+    public string? Address_2 { get; set; }
+}

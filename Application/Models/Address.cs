bc37b3eb064018b9c1061f28d1a8d16314a22526
stevejuan <91426083+stevejuan@users.noreply.github.com>
2024-04-11 21:34:38 +0300

    Add Login Model (#6)
    * Add Login Model

---
+++ Application/Models/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Models;
+
+public class Address
+{
+    public required Guid Id { get; set; }
+    public required string Address_1 { get; set; }
+    public string? Address_2 { get; set; }
+}

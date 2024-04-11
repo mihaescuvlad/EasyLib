f175b7cf18542bd4174966f0ec1b5f5239788f57
stevejuan <predastefanmarian@gmail.com>
2024-04-11 22:10:25 +0300

    Moved Context

---
+++ Application/Data/LibraryContext.cs
@@ -0,0 +1,23 @@
+﻿using Application.Models;
+
+using Microsoft.EntityFrameworkCore;
+namespace Application.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Login> Login { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        public DbSet<BookStock> BookStocks { get; set; }
+        public DbSet<BorrowHistory> BorrowHistory { get; set; }
+        public DbSet<LibraryLocation> LibraryLocations { get; set; }
+    }
+}

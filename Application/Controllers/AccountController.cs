17aae887d54633b0598fb44e6eb4dbcf4491fb4a
Talau Andrei-Ovidiu <andrei_talau@yahoo.com>
2024-04-11 15:11:27 +0300

    Create Login Panel

---
+++ Application/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Login()
+    {
+        return View("Login");
+    }
+
+    public IActionResult Register()
+    {
+        return View("Register");
+    }
+}

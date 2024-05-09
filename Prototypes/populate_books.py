322028f5f6fb3bc46d729a65d4701b6b03a381a5
mihaescuvlad <52869843+mihaescuvlad@users.noreply.github.com>
2024-05-09 20:04:00 +0300

    Update DB Setup Script

--- Prototypes/populate_books.py
+++ Prototypes/populate_books.py
@@ -8,10 +8,9 @@ def get_connection_string():
         data = json.load(f)
     server = data["ConnectionStrings"]["AZURE_CONNECTION_STRING"].split(';')[0].split('=')[1]
     database = data["ConnectionStrings"]["AZURE_CONNECTION_STRING"].split(';')[1].split('=')[1]
-    username = data["ConnectionStrings"]["AZURE_CONNECTION_STRING"].split(';')[3].split('=')[1]
-    password = data["ConnectionStrings"]["AZURE_CONNECTION_STRING"].split(';')[4].split('=')[1]
 
-    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database}'
+
     return connection_string
 
 def fetch_book_data(genre):
@@ -56,13 +55,68 @@ def insert_books(book_data):
                 cursor.execute("INSERT INTO BookAuthors (BookIsbn, AuthorId) VALUES (?, ?)",
                                (isbn, author_id))
                 conn.commit()
+
+            cursor.execute("SELECT Id FROM LibraryLocations")
+            locations = cursor.fetchall()
+            for location_id in locations:
+                cursor.execute("INSERT INTO BookStocks (BookIsbn, LibraryId, Stock) VALUES (?, ?, ?)", (isbn, location_id[0], 2))
+                conn.commit()
         else:
             print(f"Book with ISBN '{isbn}' already exists. Skipping insertion.")
 
     conn.close()
 
+def check_library_locations():
+    connection_string = get_connection_string()
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT COUNT(*) FROM LibraryLocations")
+    count = cursor.fetchone()[0]
+
+    conn.close()
+
+    return count == 0
+
+def create_library_locations():
+    if not check_library_locations():
+        print("Library locations already exist. Skipping creation.")
+        return
+
+    connection_string = get_connection_string()
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    open_time = "08:00:00"
+    close_time = "20:00:00"
+
+    libraries = [
+        {"address1": "Dolj, Craiova", "address2": "Aleea Pietrelor Metin"},
+        {"address1": "Dolj, Craiova", "address2": "Strada Sabiilor +9"},
+        {"address1": "Braila, Braila", "address2": "Taramul Sfant"},
+        {"address1": "Ierusalim", "address2": None},
+    ]
+
+    address_ids = []
+    for library in libraries:
+        address_id = str(uuid.uuid4())
+        cursor.execute("INSERT INTO Addresses (Id, Address1, Address2) VALUES (?, ?, ?)",
+                       (address_id, library["address1"], library["address2"]))
+        conn.commit()
+        address_ids.append(address_id)
+
+    for address_id in address_ids:
+        cursor.execute("INSERT INTO LibraryLocations (Id, AddressId, OpenTime, CloseTime) VALUES (?, ?, ?, ?)",
+                       (str(uuid.uuid4()), address_id, open_time, close_time))
+        conn.commit()
+
+    conn.close()
+
 def main():
     genres = ["fiction", "fantasy", "computers", "history", "science"]
+
+    create_library_locations()
+    print("Library locations created successfully.")
 
     for genre in genres:
         books = fetch_book_data(genre)
